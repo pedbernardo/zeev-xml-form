212fac5b53fcfb327333c734b102ff591e828721
Pedro Bernardo <pedrovbernardo@gmail.com>
2022-01-26 02:48:24 -0300

    feat: add config file and sample CSV data

---
+++ zeev-form-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  CODFIELD_START_AT: 16824,
+  CODDATA_START_AT: 74216,
+  CODFIELDGROUP_START_AT: 5600,
+  VERSION: 1,
+  CODFLOW: 3202,
+  DSFLOWNAME: 'Contrato Avulso',
+  CODFORM: 3929,
+  DSFORMNAME: 'PA-SESI.050 - Contratos SSI v4.0.1',
+  entryFile: './data/form.csv',
+  exportPath: './export'
+}
